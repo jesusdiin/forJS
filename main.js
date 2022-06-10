39a219fcc7285b16f73b743644dd7a82d0650a03
Jesús Hernández <cjesus02hdz@gmail.com>
2022-06-10 16:21:25 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Hacer un programa que combine sabores de helados con chispas usando for
+//  - Sabor chocolate, Vainilla
+//  - Chispas de colores, chispas de chocolate, chispas de estrellas
+
+var sabores = ['Chocolate', 'Vainilla', 'Fresa']
+var chispas = ['Chispas de Colores', 'Chispas de Chocolate', 'Chispas de Estrellas']
+
+for (let index = 0; index < sabores.length; index++) {
+    for (let j = 0; j < chispas.length; j++) {
+        console.log(`Helado sabor ${sabores[index]} con ${chispas[j]}`)    
+    }
+}
